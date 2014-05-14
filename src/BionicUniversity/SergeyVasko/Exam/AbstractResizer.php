18125ed9cce469fea4a8ea13610b8b3e13ecc39b
Sergey Vasko <sergio.vasko@gmail.com>
2014-05-14 18:03:04 +0300

    test interfaces added composer and phpunit

---
+++ src/BionicUniversity/SergeyVasko/Exam/AbstractResizer.php
@@ -0,0 +1,12 @@
+<?php
+namespace BionicUniversity\SergeyVasko\Exam;
+
+
+abstract class AbstractResizer
+{
+    /**
+     * As a result square centralized  picture should be returned
+     * @return mixed
+     */
+    abstract public function thumbnail();
+} 
---
+++ src/BionicUniversity/SergeyVasko/Exam/ImageInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BionicUniversity\SergeyVasko\Exam;
+
+
+interface ImageInterface {
+
+    public function getHeight();
+
+    public function getWidth();
+
+} 
